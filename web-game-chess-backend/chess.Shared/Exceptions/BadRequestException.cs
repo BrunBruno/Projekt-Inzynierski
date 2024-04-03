2c7d1ba0a8fe787c60e13853369f9d614e9a82d2
DESKTOP-D0EL4H2\DST <brunomaslon@gmail.com>
2024-04-03 09:27:54 +0200

    add exceptions in shared

---
+++ web-game-chess-backend/chess.Shared/Exceptions/BadRequestException.cs
@@ -0,0 +1,5 @@
+﻿
+namespace chess.Shared.Exceptions;
+public class BadRequestException : Exception {
+    public BadRequestException(string message) : base(message) { }
+}
--- web-game-chess-backend/chess.Shared/Extensions.cs
+++ web-game-chess-backend/chess.Shared/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿
+using chess.Shared.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,10 +7,14 @@ namespace chess.Shared;
 public static class Extensions {
 
     public static IServiceCollection AddShared(this IServiceCollection services) {
+        services.AddScoped<ExceptionMiddleware>();
+
         return services;
     }
 
     public static IApplicationBuilder UseShared(this IApplicationBuilder app) {
+        app.UseMiddleware<ExceptionMiddleware>();
+
         return app;
     }
 }
