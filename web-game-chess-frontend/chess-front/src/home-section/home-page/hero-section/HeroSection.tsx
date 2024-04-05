d59e6e252d92523fee1d20e737723a8e051abcab
DESKTOP-D0EL4H2\DST <brunomaslon@gmail.com>
2024-04-05 14:44:58 +0200

    add login/register section in hero

--- web-game-chess-frontend/chess-front/src/home-section/home-page/hero-section/HeroSection.tsx
+++ web-game-chess-frontend/chess-front/src/home-section/home-page/hero-section/HeroSection.tsx
@@ -1,6 +1,48 @@
-import classes from './HeroSection.module.scss';
+import classes from "./HeroSection.module.scss";
 
 const HeroSection = () => {
+  const makeGridBlock = () => {
+    const blocks = [];
+
+    for (let i = 0; i < 25; i++) {
+      blocks.push(<p key={i} />);
+    }
+
+    return (
+      <div className={classes.hero__content__extra_bg__grid}>{blocks}</div>
+    );
+  };
+
+  const generatePawns = () => {
+    const blocks = [];
+
+    const numberOfPawns = 7;
+    for (let i = 0; i < numberOfPawns; i++) {
+      const leftPercentage = (i * 100) / (numberOfPawns - 1);
+      const topPercentage =
+        (1 / 200) * Math.pow(-(leftPercentage - 100 / 2), 2) - 8;
+
+      const pawnClass = i % 2 === 0 ? "pawn-black" : "pawn-white";
+
+      blocks.push(
+        <div
+          key={i}
+          className={`${classes["img-pawn-container"]} ${classes[pawnClass]}`}
+          style={{ top: `${topPercentage}%`, left: `${leftPercentage}%` }}
+        >
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      );
+    }
+
+    return blocks;
+  };
+
   return (
     <section id="home" className={classes.hero}>
       <div className={classes.hero__content}>
@@ -13,7 +55,40 @@ const HeroSection = () => {
             exercitationem minima.
           </span>
         </div>
-        <div className={classes.hero__content__extra}></div>
+        <div className={classes.hero__content__extra_bg}>
+          {makeGridBlock()}
+          {generatePawns()}
+        </div>
+        <div className={classes.hero__content__extra}>
+          <div className={classes["hero-register"]}>
+            <div
+              className={`${classes["signin-pawn"]} ${classes["pawn-container"]}`}
+            >
+              <div className={classes["img-pawn-container"]}>
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+              </div>
+              <p>Sign In</p>
+            </div>
+            <div
+              className={`${classes["signup-pawn"]} ${classes["pawn-container"]}`}
+            >
+              <div className={classes["img-pawn-container"]}>
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+              </div>
+              <p>Sign Up</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
