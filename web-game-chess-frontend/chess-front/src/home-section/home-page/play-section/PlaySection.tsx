0571fb9032d5bbfa7e143b6cf8a6b8e2d8615ac7
DESKTOP-D0EL4H2\DST <brunomaslon@gmail.com>
2024-04-10 15:31:51 +0200

    chnage play section board

--- web-game-chess-frontend/chess-front/src/home-section/home-page/play-section/PlaySection.tsx
+++ web-game-chess-frontend/chess-front/src/home-section/home-page/play-section/PlaySection.tsx
@@ -1,3 +1,8 @@
+import {
+  streightChessboard,
+  pieceImageMap,
+} from "../../../shared/options/ChessOptions";
+
 import classes from "./PlaySection.module.scss";
 
 function PlaySection() {
@@ -27,13 +32,31 @@ function PlaySection() {
     return rows;
   };
 
+  const generatePawns = (): JSX.Element[] => {
+    const tiles: JSX.Element[] = [];
+
+    const numberOfTiles = 64;
+    for (let i = 0; i < numberOfTiles; i++) {
+      const piece = streightChessboard[i];
+      const imageUrl = pieceImageMap[piece];
+
+      tiles.push(
+        <div key={i} className={classes.pawn}>
+          {piece !== " " && <img src={`pieces/${imageUrl}`} alt={piece} />}
+          {piece !== " " && <p />}
+        </div>
+      );
+    }
+
+    return tiles;
+  };
+
   const handleOnGridHover = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
     const parentRect = event.currentTarget.getBoundingClientRect();
     const offsetX = event.clientX - parentRect.left;
     const offsetY = event.clientY - parentRect.top;
-
     const indicator = document.getElementById("indicator");
     if (indicator) {
       indicator.style.left = `${offsetX}px`;
@@ -46,12 +69,27 @@ function PlaySection() {
       <div className={classes.play__content}>
         <div className={classes.play__content__board}>
           <div className={classes.play__content__board__grid}>
-            <div
-              className={classes.play__content__board__grid__inner}
-              onMouseMove={(event) => handleOnGridHover(event)}
-            >
+            <div className={classes.play__content__board__grid__inner}>
               <div id="indicator" className={classes.indicator}></div>
               {generateGrid()}
+            </div>
+            <div
+              className={classes.play__content__board__grid__outer}
+              onMouseMove={(event) => handleOnGridHover(event)}
+              onMouseEnter={() => {
+                const indicator = document.getElementById("indicator");
+                if (indicator) {
+                  indicator.style.opacity = "1";
+                }
+              }}
+              onMouseLeave={() => {
+                const indicator = document.getElementById("indicator");
+                if (indicator) {
+                  indicator.style.opacity = "0";
+                }
+              }}
+            >
+              {generatePawns()}
             </div>
           </div>
         </div>
