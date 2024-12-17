ea58ff5a746c8c6125cfa8cc96eee3e60262da2e
DESKTOP-UC16UAE\DST BM <brunomaslon@gmail.com>
2024-12-17 10:58:49 +0100

    start game tests

--- web-game-chess-frontend/src/main/game-page/EngineGamePage.test.tsx
+++ web-game-chess-frontend/src/main/game-page/EngineGamePage.test.tsx
@@ -1,5 +1,5 @@
 import { MemoryRouter, Route, Routes } from "react-router-dom";
-import { render, waitFor, screen } from "@testing-library/react";
+import { render, waitFor, screen, fireEvent, act } from "@testing-library/react";
 import MockAdapter from "axios-mock-adapter";
 import axios from "axios";
 import EngineGamePage from "./EngineGamePage";
@@ -12,6 +12,7 @@ import {
   PieceColor,
 } from "../../shared/utils/objects/entitiesEnums";
 import { engineGameController } from "../../shared/utils/services/ApiService";
+import { taskDelay } from "../../shared/utils/functions/events";
 
 const gameId = Guid.create();
 const gameIdStr = gameId.toString();
@@ -21,19 +22,19 @@ const gameIdStr = gameId.toString();
 // responses
 
 const responseEngineGame: GetEngineGameDto = {
-  position: "",
+  position: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
   turn: 0,
   engineLevel: 1,
   hasEnded: false,
   enPassant: null,
   allowCheats: false,
   halfmoveClock: 0,
-  canWhiteKingCastle: false,
-  canWhiteShortRookCastle: false,
-  canWhiteLongRookCastle: false,
-  canBlackKingCastle: false,
-  canBlackShortRookCastle: false,
-  canBlackLongRookCastle: false,
+  canWhiteKingCastle: true,
+  canWhiteShortRookCastle: true,
+  canWhiteLongRookCastle: true,
+  canBlackKingCastle: true,
+  canBlackShortRookCastle: true,
+  canBlackLongRookCastle: true,
   player: {
     name: "",
     profilePicture: null,
@@ -77,5 +78,50 @@ describe("EngineGame Components", () => {
     );
 
     await waitFor(() => expect(screen.getByTestId("main-engine-game-page")).toBeInTheDocument());
+  });
+
+  // game actions test
+  it("should make move on user selection and update board", async () => {
+    // initial calls
+    mock.onGet(engineGameController.getEngineGame(gameId)).reply(200, responseEngineGame);
+    mock.onGet(engineGameController.getAllEngineGameMessages(gameId)).reply(200, responseMessages);
+
+    act(() => {
+      render(
+        <MemoryRouter initialEntries={[{ pathname: `/enginegame/${gameIdStr}` }]}>
+          <Routes>
+            <Route path="/enginegame/:gameIdStr" element={<EngineGamePage />} />
+          </Routes>
+        </MemoryRouter>
+      );
+    });
+
+    await act(async () => {
+      await taskDelay(1000);
+    });
+
+    console.log("after render");
+
+    // selection piece
+    const piece = await waitFor(() => screen.getByTestId("engine-game-board-field-6-2"));
+    fireEvent.click(piece);
+
+    await act(async () => {
+      await taskDelay(1000);
+    });
+
+    console.log("after piece select");
+
+    // await taskDelay(100);
+
+    // make move
+    const field = await waitFor(() => screen.getByTestId("engine-game-board-field-6-4"));
+    fireEvent.click(field);
+
+    await act(async () => {
+      await taskDelay(1000);
+    });
+
+    console.log("after field select");
   });
 });
